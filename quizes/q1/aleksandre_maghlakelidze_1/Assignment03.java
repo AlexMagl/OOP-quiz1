0dfcfcb4adf0cce9bccd94d40d74456b01294dab
AlexMagl <102256156+AlexMagl@users.noreply.github.com>
2024-03-11 11:56:36 +0400

    Add files via upload

---
+++ quizes/q1/aleksandre_maghlakelidze_1/Assignment03.java
@@ -0,0 +1,13 @@
+package quizes.q1.aleksandre_maghlakelidze_1;
+
+
+/**
+ * Find the file "hidden.txt" and print its path.
+ * The file should be found yourself without java code.
+ * 0.5 points
+ */
+public class Assignment03 {
+    public static void main(String[] args) {
+        System.out.println("\"C:\\Users\\User\\IdeaProjects\\quiz01\\src\\quizes\\q1\\aleksandre_maghlakelidze_1\\salad\\mountain\\jacket\\hidden.txt\"");
+    }
+}
---
+++ quizes/q1/aleksandre_maghlakelidze_1/iliaunirules/B.java
@@ -0,0 +1,8 @@
+package quizes.q1.aleksandre_maghlakelidze_1.iliaunirules;
+
+public class B {
+    public static void main(String[] args) {
+
+        System.out.println("This text is just for example");
+    }
+}
